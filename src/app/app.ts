385d5fe60233f62339d5440f07c6c39581129151
dmitry <oxpa97@mail.ru>
2026-07-24 13:21:05 +0300

    feat: add animated paws background to app shell

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Loader } from './shared/components/loader/loader';
 import { AppBootstrap } from './core/services/app-bootstrap/app-bootstrap';
 import { LoaderService } from './core/services/loader/loader';
+import { LottiePlayer } from './shared/components/lottie-player/lottie-player';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Loader],
+  imports: [RouterOutlet, Loader, LottiePlayer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
--- src/app/shared/components/lottie-player/lottie-animations.ts
+++ src/app/shared/components/lottie-player/lottie-animations.ts
@@ -3,6 +3,8 @@ export const LOTTIE_ANIMATIONS = {
   notFound: '404.json',
   paws: 'paws.json',
   congratulations: 'congratulations.json',
+  cats: 'cats.json',
+  pawsBg: 'paws-bg.json',
 } as const;
 
 export type LottieAnimation = keyof typeof LOTTIE_ANIMATIONS;
